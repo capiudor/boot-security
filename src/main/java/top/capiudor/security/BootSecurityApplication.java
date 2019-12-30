a4b24ac0c2986803184f73777b00434bd4eecf61
capiudor <capiudor@163.com>
2019-12-30 17:45:11 +0800

    1.构建项目（未完成配置）

---
+++ src/main/java/top/capiudor/security/BootSecurityApplication.java
@@ -0,0 +1,13 @@
+package top.capiudor.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurityApplication.class, args);
+    }
+
+}
